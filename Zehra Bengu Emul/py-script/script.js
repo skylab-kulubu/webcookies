d95d7b37c074c033c4ae8ac0824765eceeabdb2f
zehra <71967067+zehraemul@users.noreply.github.com>
2022-11-09 11:37:57 +0300

    Delete Zehra Bengu Emul/py-script directory

--- Zehra Bengu Emul/py-script/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("btn");
-const main = document.querySelector(".main");
-const img = document.querySelector(".img");
-
-btn.addEventListener("click", () => {
-  main.classList.toggle("show");
-  img.classList.toggle("show");
-  btn.classList.add("none");
-});
